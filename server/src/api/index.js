32436ae9eb88731eef2cf91313250d9e21ca947f
alexbit <bityukov.a@concord-consulting.org>
2019-05-17 22:09:45 +0300

    Added new api methods

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -17,4 +17,28 @@ module.exports = function(app) {
             res.send(users);
         });
     });
+
+    app.get(`${apiPath}/user`, function (req, res) {
+        const User = require('../db/models/user');
+        User.findOne({_id: req.query.id}).exec(function(err, user) {
+            if (err) throw err;
+            res.send(user);
+        });
+    });
+
+    app.get(`${apiPath}/products`, function (req, res) {
+        const Product = require('../db/models/product');
+        Product.find({}).exec(function(err, products) {
+            if (err) throw err;
+            res.send(products);
+        });
+    });
+
+    app.get(`${apiPath}/product`, function (req, res) {
+        const Product = require('../db/models/product');
+        Product.findOne({_id: req.query.id}).exec(function(err, product) {
+            if (err) throw err;
+            res.send(product);
+        });
+    });
 };
