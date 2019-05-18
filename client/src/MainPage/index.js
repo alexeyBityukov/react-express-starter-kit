c3a79ff9072898356a1640758f580bf26782df57
alexbit <bityukov.a@concord-consulting.org>
2019-05-18 22:20:36 +0300

    Completed top block on MainPage

--- client/src/MainPage/index.js
+++ client/src/MainPage/index.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 
-import topImage from '../../static/image/MainPage/top.jpg';
+import styles from './index.module.scss';
 
+
+const vkAlenaLink = 'https://vk.com/alenafilatowa';
 
 class MainPage extends Component {
     render() {
         return (
-            <div>
-                MainPage
-                <img src={topImage} />
+            <div className={styles.root}>
+                <div className={styles.top}>
+                    <div className={styles.client}>
+                        <a href={vkAlenaLink}>Филатова Алена :)</a>
+                    </div>
+                    <div className={styles.title}>
+                        Добро пожаловать к Алене!
+                    </div>
+                    <div className={styles.subtitle}>
+                        Лучший интернет магазин по продаже обуви
+                    </div>
+                    <div className={styles.showProducts}>
+                        Просмотреть асортимент
+                    </div>
+                </div>
             </div>
         );
     }
