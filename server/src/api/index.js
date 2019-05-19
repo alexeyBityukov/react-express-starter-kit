627e502577f1d23d4c7459d6a2808ed3ee9308b4
alexbit <bityukov.a@concord-consulting.org>
2019-05-19 23:05:37 +0300

    Added correct fix to CORS

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -3,10 +3,11 @@ const apiPath = '/api';
 
 
 module.exports = function(app) {
-    app.get(`${apiPath}/*`,function(req,res,next){
-        res.header('Access-Control-Allow-Origin' , 'http://localhost:3000' );
-        res.header('Access-Control-Allow-Origin' , 'http://localhost:5000' );
-        next(); // http://expressjs.com/guide.html#passing-route control
+    app.get(`${apiPath}/*`,function(req, res, next){
+        console.log(req.headers.origin);
+        if(req.headers.origin === 'http://localhost:3000' || req.headers.origin === 'http://localhost:5000')
+            res.set('Access-Control-Allow-Origin' , req.headers.origin );
+        next();
     });
 
     app.get(`${apiPath}/users`, function (req, res) {
