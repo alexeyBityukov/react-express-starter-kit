45fb7e09c18fd60566653aa0818b50dc75bac117
alexbit <bityukov.a@concord-consulting.org>
2019-05-19 22:52:14 +0300

    Fix CORS

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,6 +1,14 @@
 const apiPath = '/api';
 
+
+
 module.exports = function(app) {
+    app.get(`${apiPath}/*`,function(req,res,next){
+        res.header('Access-Control-Allow-Origin' , 'http://localhost:3000' );
+        res.header('Access-Control-Allow-Origin' , 'http://localhost:5000' );
+        next(); // http://expressjs.com/guide.html#passing-route control
+    });
+
     app.get(`${apiPath}/users`, function (req, res) {
         const User = require('../db/models/user');
         User.find({}).exec(function(err, users) {
